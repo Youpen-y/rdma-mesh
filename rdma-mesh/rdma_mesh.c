38dc5e166d872e79161ccafd251945ed35f6e96d
Youpen-y <yongy2022@outlook.com>
2025-01-14 09:09:32 +0800

    Update: reimplement the retry mechanism
    - release the unavailable resources

--- rdma-mesh/rdma_mesh.c
+++ rdma-mesh/rdma_mesh.c
@@ -8,12 +8,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
 #include "rdma_mesh.h"
 
 #define MAX_HOSTS 16
 
 #define DEFAULT_PORT 40000
 #define MAX_CONNECTIONS (MAX_HOSTS * (MAX_HOSTS-1) / 2)
+#define MAX_RETRY 80
 
 extern struct host_context ctx;
 
@@ -151,6 +153,8 @@ void *run_client(void *arg) {
     struct ibv_qp_init_attr qp_attr;
     struct rdma_conn_param conn_param;
     int ret;
+    bool retry_flag = true;
+    int retry_count = 0;
 
     // 创建事件通道
     ec = rdma_create_event_channel();
@@ -159,95 +163,106 @@ void *run_client(void *arg) {
         return NULL;
     }
 
-    // 简单的重传（之后再修改）
-retry:
-    // 创建RDMA CM ID
-    ret = rdma_create_id(ec, &id, NULL, RDMA_PS_TCP);
-    if (ret) {
-        fprintf(stderr, "Host %d: Failed to create RDMA CM ID for client[%d]\n", ctx.host_id, target_host);
-        goto cleanup;
-    }
-
-    // 连接到目标主机
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(DEFAULT_PORT + target_host);
-    addr.sin_addr.s_addr = inet_addr("192.168.103.2");
-
-    ret = rdma_resolve_addr(id, NULL, (struct sockaddr *)&addr, 2000);
-    if (ret) {
-        fprintf(stderr, "Host %d: Failed to resolve address for host %d\n", ctx.host_id, target_host);
-        goto cleanup;
-    }
-
-    while (1) {
-        ret = rdma_get_cm_event(ec, &event);
+    while(retry_flag && retry_count < MAX_RETRY) {
+        retry_flag = false;
+        // 创建RDMA CM ID
+        ret = rdma_create_id(ec, &id, NULL, RDMA_PS_TCP);
         if (ret) {
-            continue;
+            fprintf(stderr, "Host %d: Failed to create RDMA CM ID for client[%d]\n", ctx.host_id, target_host);
+            goto cleanup;
         }
 
-        switch (event->event) {
-            case RDMA_CM_EVENT_ADDR_RESOLVED:
-                ret = rdma_resolve_route(event->id, 2000);
-                break;
+        // 连接到目标主机
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(DEFAULT_PORT + target_host);
+        addr.sin_addr.s_addr = inet_addr("192.168.103.2");
 
-            case RDMA_CM_EVENT_ROUTE_RESOLVED:
-                memset(&qp_attr, 0, sizeof(qp_attr));
-                qp_attr.qp_context = NULL;
-                qp_attr.cap.max_send_wr = 10;
-                qp_attr.cap.max_recv_wr = 10;
-                qp_attr.cap.max_send_sge = 1;
-                qp_attr.cap.max_recv_sge = 1;
-                qp_attr.cap.max_inline_data = 64;
-                qp_attr.qp_type = IBV_QPT_RC;
-                qp_attr.send_cq = ibv_create_cq(event->id->verbs, 10, NULL, NULL, 0);
-                qp_attr.recv_cq = qp_attr.send_cq;
-
-                ret = rdma_create_qp(event->id, NULL, &qp_attr);
-                if (!ret) {
-                    memset(&conn_param, 0, sizeof(conn_param));
-                    
-                    // 设置私有数据（即发起连接方的身份标识）
-                    conn_param.private_data = &ctx.host_id;
-                    conn_param.private_data_len = sizeof(ctx.host_id);
-                    conn_param.initiator_depth = 1;
-
-                    // 设置资源参数
-                    conn_param.responder_resources = 2;     // 可同时处理 2 个 RDMA Read
-                    conn_param.initiator_depth = 2;         // 可以发起 2 个并发的 RDMA Read
-                    conn_param.flow_control = 1;            // 启用流控
-                    conn_param.retry_count = 7;             // 发送重传 7 次
-                    conn_param.rnr_retry_count = 7;         // RNR 重传 7 次
-
-                    // 不使用 SRQ ，使用系统分配的 QP 号
-                    conn_param.srq = 0;
-                    conn_param.qp_num = 0;
-           
-                    ret = rdma_connect(event->id, &conn_param);
-                }
-                break;
-
-            case RDMA_CM_EVENT_REJECTED:    // 当 client 早于远端 server 建立时，返回 RDMA_CM_EVENT_REJECTED
-                printf("Connect to host %d failed, event: %s", target_host, rdma_event_str(event->event));
-                goto retry;
-
-            case RDMA_CM_EVENT_ESTABLISHED:
-                printf("Host %d: Connected to host %d\n", ctx.host_id, target_host);
-                cm_id_array[target_host] = *(event->id);
-                // 这里可以通过 event->param.conn.private_data 查看 server 返回的私有数据
-                printf("Connection setup with host %d\n", *(int *)event->param.conn.private_data);
-                goto cleanup;
-
-            case RDMA_CM_EVENT_CONNECT_ERROR:
-            case RDMA_CM_EVENT_UNREACHABLE:
-            case RDMA_CM_EVENT_DISCONNECTED:
-                goto cleanup;
-
-            default:
-                break;
+        ret = rdma_resolve_addr(id, NULL, (struct sockaddr *)&addr, 2000);
+        if (ret) {
+            fprintf(stderr, "Host %d: Failed to resolve address for host %d\n", ctx.host_id, target_host);
+            goto cleanup;
         }
 
-        rdma_ack_cm_event(event);
+        while (1) {
+            ret = rdma_get_cm_event(ec, &event);
+            if (ret) {
+                continue;
+            }
+
+            switch (event->event) {
+                case RDMA_CM_EVENT_ADDR_RESOLVED:
+                    ret = rdma_resolve_route(event->id, 2000);
+                    break;
+
+                case RDMA_CM_EVENT_ROUTE_RESOLVED:
+                    memset(&qp_attr, 0, sizeof(qp_attr));
+                    qp_attr.qp_context = NULL;
+                    qp_attr.cap.max_send_wr = 10;
+                    qp_attr.cap.max_recv_wr = 10;
+                    qp_attr.cap.max_send_sge = 1;
+                    qp_attr.cap.max_recv_sge = 1;
+                    qp_attr.cap.max_inline_data = 64;
+                    qp_attr.qp_type = IBV_QPT_RC;
+                    qp_attr.send_cq = ibv_create_cq(event->id->verbs, 10, NULL, NULL, 0);
+                    qp_attr.recv_cq = qp_attr.send_cq;
+
+                    ret = rdma_create_qp(event->id, NULL, &qp_attr);
+                    if (!ret) {
+                        memset(&conn_param, 0, sizeof(conn_param));
+                        
+                        // 设置私有数据（即发起连接方的身份标识）
+                        conn_param.private_data = &ctx.host_id;
+                        conn_param.private_data_len = sizeof(ctx.host_id);
+                        conn_param.initiator_depth = 1;
+
+                        // 设置资源参数
+                        conn_param.responder_resources = 2;     // 可同时处理 2 个 RDMA Read
+                        conn_param.initiator_depth = 2;         // 可以发起 2 个并发的 RDMA Read
+                        conn_param.flow_control = 1;            // 启用流控
+                        conn_param.retry_count = 7;             // 发送重传 7 次
+                        conn_param.rnr_retry_count = 7;         // RNR 重传 7 次
+
+                        // 不使用 SRQ ，使用系统分配的 QP 号
+                        conn_param.srq = 0;
+                        conn_param.qp_num = 0;
+            
+                        ret = rdma_connect(event->id, &conn_param);
+                    }
+                    break;
+
+                case RDMA_CM_EVENT_REJECTED:    // 当 client 早于远端 server 建立时，返回 RDMA_CM_EVENT_REJECTED
+                    printf("Connect to host %d failed, event: %s", target_host, rdma_event_str(event->event));
+                    // if connected failed, release the qp and id (need to reconsider)
+                    if (retry_count < MAX_RETRY) {
+                        retry_count++;
+                        retry_flag = true;
+                    }
+                    if (id) {
+                        rdma_destroy_qp(id);
+                        rdma_destroy_id(id);
+                    }
+                    id = NULL;  // 重置 id
+                    break;
+
+                case RDMA_CM_EVENT_ESTABLISHED:
+                    printf("Host %d: Connected to host %d\n", ctx.host_id, target_host);
+                    cm_id_array[target_host] = *(event->id);
+                    // 这里可以通过 event->param.conn.private_data 查看 server 返回的私有数据
+                    printf("Connection setup with host %d\n", *(int *)event->param.conn.private_data);
+                    goto cleanup;
+
+                case RDMA_CM_EVENT_CONNECT_ERROR:
+                case RDMA_CM_EVENT_UNREACHABLE:
+                case RDMA_CM_EVENT_DISCONNECTED:
+                    goto cleanup;
+
+                default:
+                    break;
+            }
+
+            rdma_ack_cm_event(event);
+        }
     }
 
 cleanup:
